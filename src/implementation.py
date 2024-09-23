87ddbfb22fc0c9b82a3622c349584637590fd3e2
andres <andresnowak52@gmail.com>
2024-09-23 13:05:25 +0200

    Added gd and sgd

--- src/implementation.py
+++ src/implementation.py
@@ -49,3 +49,59 @@ def ridge_regression(y, tx, lambda_):
     w_ridge = np.linalg.solve(A, b)
 
     return w_ridge
+
+
+def compute_gradient(y, tx, w):
+    """Computes the gradient at w.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        w: shape=(2, ). The vector of model parameters.
+
+    Returns:
+        An array of shape (2, ) (same shape as w), containing the gradient of the loss at w.
+    """
+
+    # MSE
+
+    error = y - tx @ w
+    return - 1/tx.shape[0] * error @ tx
+
+
+def gradient_descent(y, tx, initial_w, max_iters, gamma):
+    """The Gradient Descent (GD) algorithm.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        initial_w: shape=(2, ). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of GD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        losses: a list of length max_iters containing the loss value (scalar) for each iteration of GD
+        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of GD
+    """
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        # compute gradient and loss
+        loss = compute_loss(y, tx, w)
+        w_g = compute_gradient(y, tx, w)
+
+        # update w by gradient
+        w = w - gamma * w_g
+
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+        print(
+            "GD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
+                bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
+            )
+        )
+
+    return losses, ws
