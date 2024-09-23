9f7b6d83a3a3c0c18d0c741a8dbd3bf1e2fc47bc
andres <andresnowak52@gmail.com>
2024-09-23 17:08:52 +0200

    Changed re_log_regression

--- implementations.py
+++ implementations.py
@@ -225,7 +225,7 @@ def logistic_regression(y, tx, w, max_iter, gamma):
 def compute_loss_reg(y, tx, w, lambda_):
     """Compute the regularized logistic regression loss."""
     loss = calculate_loss(y, tx, w)
-    return loss + lambda_*np.sum(w**2)
+    return loss + lambda_*np.sum(w.T @ w)
    
 def compute_gradient_reg(y, tx, w, lambda_):
     """Compute the regularized gradient for logistic regression."""
